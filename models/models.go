e501a5df4073618095464326f6b3bd158f134288
Muhammad Arief <azkklk@gmail.com>
2023-10-26 23:02:29 +0800

    refactor(model): refactoring User and Photo models

--- models/models.go
+++ models/models.go
@@ -6,11 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	ID        uint		  `gorm:"primaryKey,not null" json:"id,omitempty"`
-	Username  string 		`gorm:"not null" json:"username,omitempty"`
-	Email     string    `gorm:"unique,not null" json:"email,omitempty"`
-	Password  string    `gorm:"not null,size:>6" json:"password,omitempty"`
-	Photo     Photo     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID       uint   `gorm:"primaryKey,not null" json:"id,omitempty"`
+	Username string `gorm:"not null" json:"username,omitempty"`
+	Email    string `gorm:"unique,not null" json:"email,omitempty"`
+	Password string `gorm:"not null,size:>6" json:"password,omitempty"`
+	Photo    Photo  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type Photo struct {
