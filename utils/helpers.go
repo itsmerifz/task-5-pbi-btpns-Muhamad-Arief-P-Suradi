47bb01c3abac1ec1350d35c44d78c19cac470ac4
Muhammad Arief <azkklk@gmail.com>
2023-10-26 23:03:16 +0800

    add(helper): add ValidUser function

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,6 +1,9 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"golang.org/x/crypto/bcrypt"
+	valid "github.com/asaskevich/govalidator"
+)
 
 func HashPassword(password string) string {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -13,4 +16,11 @@ func HashPassword(password string) string {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func ValidUser(email string, password string) bool {
+	if valid.IsEmail(email) && len(password) >= 6 {
+		return true
+	}
+	return false
 }
