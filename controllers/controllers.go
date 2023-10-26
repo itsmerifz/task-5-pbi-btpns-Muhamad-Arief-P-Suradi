df2c099b1a93153e0b57d32e77b9ab8b179bbae2
Muhammad Arief <azkklk@gmail.com>
2023-10-26 22:02:34 +0800

    feat(jwt): add middleware and login handler

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,8 +7,14 @@ import (
 	"rkpbi-go/models"
 	"rkpbi-go/utils"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func (database *Database) CreateUser(c *gin.Context) {
 	var (
@@ -45,5 +51,67 @@ func (database *Database) CheckUser(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"result": query.Rows})
+	c.JSON(http.StatusOK, gin.H{"result": user})
+}
+
+func (database *Database) LoginHandler(c *gin.Context) {
+	var user Credentials
+	users := models.User{}
+	err := c.Bind(&user)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  http.StatusBadRequest,
+			"message": "Invalid request",
+		})
+	}
+
+	// Search user in database
+	userResult := database.DB.First(&users, "username = ?", user.Username)
+	if userResult != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "User not found",
+		})
+		c.Abort()
+		return
+	} else {
+		if users.Username == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"status":  http.StatusUnauthorized,
+				"message": "Invalid credentials",
+			})
+			c.Abort()
+			return
+		} else {
+			// Compare password
+			if !utils.CheckPasswordHash(user.Password, users.Password) {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"status":  http.StatusUnauthorized,
+					"message": "Invalid credentials",
+				})
+				c.Abort()
+				return 
+			}
+		}
+	}
+
+	sign := jwt.New(jwt.GetSigningMethod("HS256"))
+	token, err := sign.SignedString([]byte("secret"))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  http.StatusInternalServerError,
+			"message": "Internal server error",
+		})
+		c.Abort()
+		return
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusOK,
+			"message": "Login success",
+			"token":   token,
+		})
+		return
+	}
+
 }
