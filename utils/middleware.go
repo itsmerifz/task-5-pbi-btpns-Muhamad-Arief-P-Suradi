394494d0e14160ef9a531f22f6485bad28056cb7
Muhammad Arief <azkklk@gmail.com>
2023-10-26 23:02:51 +0800

    add(middleware): add AuthMiddleware function

--- utils/middleware.go
+++ utils/middleware.go
@@ -1,1 +1,30 @@
 package utils
+
+import (
+	"fmt"
+	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(c *gin.Context) {
+	tokenString := c.Request.Header.Get("Authorization")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if jwt.GetSigningMethod("HS256") != token.Method {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+
+	if token != nil && err == nil {
+		fmt.Println("token verified")
+	} else {
+		result := gin.H{
+			"message": "not authorized",
+			"error":   err.Error(),
+		}
+		c.JSON(http.StatusUnauthorized, result)
+		c.Abort()
+	}
+}
