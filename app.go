d608440fefe2127c25d2b860e0894cba64b642fa
Muhammad Arief <azkklk@gmail.com>
2023-10-26 23:01:48 +0800

    feat(main): add init user struct

--- app.go
+++ app.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	ID       uint   `json:"id,omitempty"`
+	Username string `json:"username,omitempty"`
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
 func main() {
 	routers := gin.Default()
 	router.UserRouter(routers)
