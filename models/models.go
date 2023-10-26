c451562c3dd493b1b338a738ec3a62e69f9ea6bf
Muhammad Arief <azkklk@gmail.com>
2023-10-26 23:52:35 +0800

    refactor(model): refactoring models

--- models/models.go
+++ models/models.go
@@ -1,16 +1,20 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	ID       uint   `gorm:"primaryKey,not null" json:"id,omitempty"`
-	Username string `gorm:"not null" json:"username,omitempty"`
-	Email    string `gorm:"unique,not null" json:"email,omitempty"`
-	Password string `gorm:"not null,size:>6" json:"password,omitempty"`
-	Photo    Photo  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID        uint   `gorm:"primaryKey,not null, autoIncrement" json:"id,omitempty"`
+	Username  string `gorm:"not null" json:"username,omitempty"`
+	Email     string `gorm:"unique,not null" json:"email,omitempty"`
+	Password  string `gorm:"not null,size:>6" json:"password,omitempty"`
+	Photo     Photo  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Photo struct {
@@ -20,4 +24,6 @@ type Photo struct {
 	Caption  string
 	PhotoUrl string
 	UserID   uint
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
