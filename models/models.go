e109308a901f80f7ae8b1de700fe322b567f49fc
Muhammad Arief <azkklk@gmail.com>
2023-10-28 00:09:40 +0800

    fix(model): add gorm foreign key statement in User model

--- models/models.go
+++ models/models.go
@@ -8,22 +8,22 @@ import (
 
 type User struct {
 	gorm.Model
-	ID        uint   `gorm:"primaryKey,not null, autoIncrement" json:"id,omitempty"`
-	Username  string `gorm:"not null" json:"username,omitempty"`
-	Email     string `gorm:"unique,not null" json:"email,omitempty"`
-	Password  string `gorm:"not null,size:>6" json:"password,omitempty"`
-	Photo     Photo  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID        uint    `gorm:"primaryKey,not null, autoIncrement" json:"id,omitempty"`
+	Username  string  `gorm:"not null" json:"username,omitempty"`
+	Email     string  `gorm:"unique,not null" json:"email,omitempty"`
+	Password  string  `gorm:"not null,size:>6" json:"password,omitempty"`
+	Photo     []Photo `gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
 type Photo struct {
 	gorm.Model
-	ID       uint `gorm:"primaryKey;not null"`
-	Title    string
-	Caption  string
-	PhotoUrl string
-	UserID   uint
+	ID        uint `gorm:"primaryKey;not null"`
+	Title     string
+	Caption   string
+	PhotoUrl  string
+	UserID    uint
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
