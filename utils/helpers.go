41d27bad54eb5b1f83bfd6b574eb54e8d00568ac
Muhammad Arief <azkklk@gmail.com>
2023-10-27 10:14:34 +0800

    add(helpers): add JWT functions

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,9 +1,19 @@
 package utils
 
 import (
-	"golang.org/x/crypto/bcrypt"
+	"errors"
+	"time"
 	valid "github.com/asaskevich/govalidator"
+	jwt "github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type CustomJWTClaims struct {
+	UserID string `json:"id"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("secret")
 
 func HashPassword(password string) string {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
@@ -23,4 +33,67 @@ func ValidUser(email string, password string) bool {
 		return true
 	}
 	return false
+}
+
+func ValidPhoto(title string, caption string, photoUrl string) bool {
+	if valid.IsAlphanumeric(title) && valid.IsAlphanumeric(caption) && valid.IsURL(photoUrl) {
+		return true
+	}
+	return false
+}
+
+func GenerateJWT(id string) (token string, err error) {
+	expTime := time.Now().Add(time.Hour * 24).Unix()
+	claims := &CustomJWTClaims{
+		UserID: id,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expTime,
+		},
+	}
+	sign := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), claims)
+	token, err = sign.SignedString(jwtKey)
+	return
+}
+
+func ValidateToken(tokenString string) (err error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&CustomJWTClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		},
+	)
+	if err != nil {
+		return
+	}
+	claims, ok := token.Claims.(*CustomJWTClaims)
+	if !ok {
+		err = errors.New("token is invalid")
+		return
+	}
+	if claims.ExpiresAt < time.Now().Unix() {
+		err = errors.New("token is expired")
+		return
+	}
+	return
+}
+
+func GetUserIDFromToken(tokenString string) (id string, err error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&CustomJWTClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		},
+	)
+	if err != nil {
+		return
+	}
+	claims, ok := token.Claims.(*CustomJWTClaims)
+	if !ok {
+		err = errors.New("token is invalid")
+		return
+	}
+	id = claims.UserID
+	return
 }
